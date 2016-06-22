ab2ea3fac22dd5df81aa5763f4dd7b1ae17e6f9d
moukaddam <mhd.moukaddam@gmail.com>
2016-06-22 15:06:43 -0700

    changed the design a bit, fixed a bug when reading lines starting with zero, chi2 analysis to be repeated

--- Mapper.cpp
+++ Mapper.cpp
@@ -34,38 +34,26 @@ cout << " chi2 " << chi2 << endl;
 double mapping(TString SimData, TString ExpData, TString background = "nobackground"){
 
 /////////////////////// OPEN INPUT AND OUTPUT FILES ///////////////////////
-
-SimulationPoint* SimPoint = new SimulationPoint();
-
-// create a TH3D
-TH3D *f3DHistBx = new TH3D("f3DHistBX", "Bx"         , 111,-111, 111, 111,-111, 111, 106, -106, 106); 
-TH3D *f3DHistBy = new TH3D("f3DHistBY", "By"         , 111,-111, 111, 111,-111, 111, 106, -106, 106);  
-TH3D *f3DHistBz = new TH3D("f3DHistBZ", "Bz"         , 111,-111, 111, 111,-111, 111, 106, -106, 106);  
-TH3D *f3DHistBmag = new TH3D("f3DHistBMAG", "Bmag"   , 111,-111, 111, 111,-111, 111, 106, -106, 106);     
-TH3D *f3DHistBtan = new TH3D("f3DHistBTAN", "Btan"   , 111,-111, 111, 111,-111, 111, 106, -106, 106);  
-TH3D *f3DHistBdiff = new TH3D("f3DHistBDIFF", "Bdiff", 111,-111, 111, 111,-111, 111, 106, -106, 106); 
-    
-    
-///////////////////////////// OPEN INPUT AND OUTPUT FILES ///////////////////////
 // open the data file
 ifstream input_exp;
-TString fullpathtofile = Form("./input/%s",ExpData.Data() ) ; cout<<"opening file : |"<<ExpData<< "| ... ";
+TString fullpathtofile = Form("%s",ExpData.Data() ) ; cout<<"opening file : |"<<ExpData<< "| ... ";
 input_exp.open( ExpData.Data() );
 if (!input_exp) { cout<<"problem opening experimental data file " << fullpathtofile << endl; exit(-1);}
 else cout<<"Experimental file is opened "<<endl;
 
 //open the field files from comsol  
 ifstream input_sim;
-fullpathtofile = Form("./input/%s",SimData.Data() ) ; 
+fullpathtofile = Form("%s",SimData.Data() ) ; 
 cout<<"opening file : |"<<SimData<< "| ... ";
 input_sim.open(SimData.Data() ) ; // from local input if not go seek in data 1
 if(!input_sim.is_open()) {
 	cout<<"problem opening simulation data file |"<< fullpathtofile <<"|"<< endl;
 	SimData.ReplaceAll("./input/","");
-	fullpathtofile = Form("/data1/moukaddam/MagnetsMapping/%s",SimData.Data()) ;
+	fullpathtofile = Form("/data1/moukaddam/MagnetSimulation/%s",SimData.Data()) ;
 	cout<<"trying main directory, opening file : |"<<fullpathtofile<< "| ... ";
+	input_sim.open(fullpathtofile.Data() ) ; // from local input if not go seek in data 1
+	if (!input_sim.is_open()) { cout<<"problem opening simulation data file |"<< fullpathtofile <<"|"<< endl<<endl; exit(-1);}
 }
-if (!input_sim.is_open()) { cout<<"problem opening simulation data file |"<< fullpathtofile <<"|"<< endl; exit(-1);}
 else cout<<" Simulation file " <<  fullpathtofile <<  " is opened " <<endl;
 
 // create root output file 
@@ -79,20 +67,24 @@ dirquad[1] = outputFile.mkdir("Quad_2");
 dirquad[2] = outputFile.mkdir("Quad_3");
 dirquad[3] = outputFile.mkdir("Quad_4");
 
-///////////////////////////// 
-/////////////////////////////  READ THE SIMULATION FILE ///////////////////////
-///////////////////////////// 
-
+//Read the simulation file from comsol 
 // dump the first lines from the top
 string s_buffer="buffer";
 Int_t d_buffer=-1;
 Int_t counter1=0; // counter on the first lines
 Int_t counter2=0; // counter on the first lines
 // read the first lines
-Int_t dimx_2, dimy_2, dimz_2;
-input_sim>>dimx_2>>dimy_2>>dimz_2;
-cout<<"                                    "<<" X "<<" Y "<<" Z "<<endl;
-cout<<" Simulated Field Table dimensions : "<<dimx_2<<"  "<<dimy_2<<"  "<<dimz_2<<endl;
+Int_t dimX, dimY, dimZ;
+input_sim>>dimX>>dimY>>dimZ;
+cout<<"\nSimulated Field Table dimensions      X:"<<dimX<<"     Y:"<<dimY<<"    Z:"<<dimZ<<endl;
+
+TH3D *f3DHistBx = new TH3D("f3DHistBX", "Bx"         , dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ); 
+TH3D *f3DHistBy = new TH3D("f3DHistBY", "By"         , dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ);  
+TH3D *f3DHistBz = new TH3D("f3DHistBZ", "Bz"         , dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ);  
+TH3D *f3DHistBmag = new TH3D("f3DHistBMAG", "Bmag"   , dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ);     
+TH3D *f3DHistBtan = new TH3D("f3DHistBTAN", "Btan"   , dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ);  
+TH3D *f3DHistBdiff = new TH3D("f3DHistBDIFF", "Bdiff", dimX,-dimX, dimX, dimY,-dimY, dimY, dimZ, -dimZ, dimZ); 
+
 d_buffer=-1;
 while (d_buffer != 0 && counter1< 15){ // find a solution for this with do while
 	input_sim>>d_buffer>>s_buffer;
@@ -101,8 +93,8 @@ while (d_buffer != 0 && counter1< 15){ // find a solution for this with do while
 getline(input_sim,s_buffer);
 cout<< " Number of skipped lines in comsol file : " <<counter2 << "  last line content : "<<s_buffer<<endl;
 
-
 // read and fill
+SimulationPoint* SimPoint = new SimulationPoint();
 Double_t X(0), Y(0), Z(0), EX(-100), EY(-100), EZ(-100), Perm(0);
 Double_t BX(0), BY(0), BZ(0);
 Int_t line=0;
@@ -110,16 +102,11 @@ Int_t line=0;
 while ( !input_sim.eof() ) {
 	   //Clear parameters
 	   SimPoint->ClearParameters();
-	   
 	   // Choose format
 	   if(counter2<9)   input_sim >> X >> Y >> Z >> BX >> BY >> BZ >> Perm ;
 	   else   			input_sim >> X >> Y >> Z >> BX >> BY >> BZ >> EX >> EY >> EZ >> Perm ;
-	   
-		// Read the line, apply offset and do some modifications
-		//Z = Z + 4 ; //Simulation is offset by -4mm, this corrects for it
 		SimPoint->ReadLineAndTreat(X,Y,Z,BX,BY,BZ,EX,EY,EZ,Perm );
 		//SimPoint->Show();
-
 		// fill in TH3D all the simulation data
 		Int_t binNumber = f3DHistBx->FindBin(X,Y,Z);
 		f3DHistBx->SetBinContent(binNumber,BX);
@@ -147,10 +134,7 @@ SimManager *SimBtanManager = new SimManager(f3DHistBtan,0.38/*mm*/,0.38/*mm*/,0.
 SimManager *SimBdifManager = new SimManager(f3DHistBdiff,0.38/*mm*/,0.38/*mm*/,0.38/*mm*/);
 
 
-///////////////////////////// 
-/////////////////////////////  READ THE DATA FILE ///////////////////////
-///////////////////////////// 
-
+// read the data file 
 //create the map holding all the slices in z
 map<Double_t,ExpManager> mapExpField;
 mapExpField[0].SetInfo("Bx"); 
@@ -162,8 +146,8 @@ mapExpField[4].SetInfo("Btan"); // tangential field
 ExperimentalPoint* ExpPoint = new ExperimentalPoint(background);  
 // Attention, Sensor X,Y or Z measures in all directions (not necessarly what the 
 // name suggest). This is due to the fact that lens is rotated with respect to mapper plate in some cases
+// The ExperimentalPoint Class will take care fo all these rotations once the location of the measurment is provided 
 
-//FORMAT : 1	A	1	3	-904.5	568.7	232.7
 TString  Grid=" ";
 Double_t Quadrant(0);
 Double_t Level(0);
@@ -179,12 +163,15 @@ for (int i = 0 ; i < 1 ; i++){ // number of lines to neglect
 
 double R,Theta,Bexp,Bsim,Berr;
 while (input_exp >> Quadrant){
-	if (Quadrant == 0) continue ; 	//skip lines starting by zero
+	cout << "Quadrant : " << Quadrant <<endl;
+	if (Quadrant == 0) { getline(input_exp,s_buffer); continue ;} 	//skip lines starting by zero
+	//FORMAT : 1	A	1	3	-904.5	568.7	232.7
 	input_exp >> Grid >> Position >> Level >> BX >> BY >> BZ;
+	  cout << Grid <<  Position << Level << BX << BY << BZ<<endl; 
 
 	ExpPoint->ClearParameters();	
 	ExpPoint->ReadLineAndTreat( Quadrant, Grid, Position, Level , BX , BY , BZ); 
-    
+	//ExpPoint->ShowParameters(); 
 
 	X = ExpPoint->fSensorPositionX.X() ; 
     Y = ExpPoint->fSensorPositionX.Y() ; 
@@ -197,7 +184,6 @@ while (input_exp >> Quadrant){
 	//
 	mapExpField[0].FillValue(X,Y,Z,R,Theta,Bexp,Bsim);
 	mapExpField[0].FillValueError(0.5,0.5,0.5,0.7,0.5,Berr);
-
 
 	X = ExpPoint->fSensorPositionY.X() ; 
     Y = ExpPoint->fSensorPositionY.Y() ; 
@@ -258,7 +244,7 @@ for (unsigned i = 0 ; i <ExpPoint->flistGrid.size() ; i++) {
     TString grid = ExpPoint->flistGrid.at(i);     
     int quad = ExpPoint->flistQuad.at(i);    
     double depth = ExpPoint->flistDepth.at(i);  
-    cout << " Drawing graphs for      Grid :" << grid << "     Quad : " << quad << "     depth : " << depth << endl ; 
+    //cout << " Drawing graphs for      Grid :" << grid << "     Quad : " << quad << "     depth : " << depth << endl ; 
     
 	outputFile.cd(); 
     dirquad[quad-1]->cd();
@@ -267,101 +253,17 @@ for (unsigned i = 0 ; i <ExpPoint->flistGrid.size() ; i++) {
 	    dir = dirquad[quad-1]->mkdir(Form("Depth_%.2f_Grid_",depth)+grid); 
 	dir->cd(); 
 
-    mapExpField.at(0).DrawGraphs(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
-    mapExpField.at(1).DrawGraphs(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
-    mapExpField.at(2).DrawGraphs(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
+    mapExpField.at(0).DrawGraphs(grid, quad, depth);
+    mapExpField.at(1).DrawGraphs(grid, quad, depth);
+    mapExpField.at(2).DrawGraphs(grid, quad, depth);
 
-    SimBxManager->DrawGraphs(ExpPoint->GetOffsetDirection(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), "X"), ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
-    SimByManager->DrawGraphs(ExpPoint->GetOffsetDirection(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), "Y"), ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
-    SimBzManager->DrawGraphs(ExpPoint->GetOffsetDirection(ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), "Z"), ExpPoint->flistGrid.at(i), ExpPoint->flistQuad.at(i), ExpPoint->flistDepth.at(i));
+    SimBxManager->DrawGraphs(ExpPoint->GetOffsetDirection(grid, quad, "X"), grid, quad, depth);
+    SimByManager->DrawGraphs(ExpPoint->GetOffsetDirection(grid, quad, "Y"), grid, quad, depth);
+    SimBzManager->DrawGraphs(ExpPoint->GetOffsetDirection(grid, quad, "Z"), grid, quad, depth);
 }
 
-
-/*
-mapExpField.at(0).DrawMap("Bx",-100,+100,-100,+100,-100,+100) ;  
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,22.5-5,22.5+5);
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,67.5-5,67.5+5);
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,22.5-5,22.5+5); 
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,67.5-5,67.5+5);
-
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,90+22.5-5,90+22.5+5);
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(0).GetExp1DGraphPolar("Bx",-44.9,-44.7,90+67.5-5,90+67.5+5);
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,90+22.5-5,90+22.5+5);
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(0).GetSim1DGraphPolar("Bx",-44.9,-44.7,90+67.5-5,90+67.5+5);
-
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,22.5-5,22.5+5);
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,67.5-5,67.5+5);
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,22.5-5,22.5+5); 
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,67.5-5,67.5+5);
-
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,90+22.5-5,90+22.5+5);
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(1).GetExp1DGraphPolar("By",-44.9,-44.7,90+67.5-5,90+67.5+5);
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,90+22.5-5,90+22.5+5); 
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(1).GetSim1DGraphPolar("By",-44.9,-44.7,90+67.5-5,90+67.5+5);
-
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,22.5-5,22.5+5);
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,67.5-5,67.5+5);
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,22.5-5,22.5+5); 
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,45.0-5,45.0+5);
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,67.5-5,67.5+5);
-
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,90+22.5-5,90+22.5+5);
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(2).GetExp1DGraphPolar("Bz",-44.9,-44.7,90+67.5-5,90+67.5+5);
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,90+22.5-5,90+22.5+5); 
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,90+45.0-5,90+45.0+5);
-mapExpField.at(2).GetSim1DGraphPolar("Bz",-44.9,-44.7,90+67.5-5,90+67.5+5);
-
-
-//mapExpField.at(0).GetExp2DGraph("Bx",-100,+100,-100,+100,-100,+100) ;
-//mapExpField.at(1).GetExp2DGraph("By",-100,+100,-100,+100,-100,+100) ;
-//mapExpField.at(3).GetExp2DGraph("Bmag",-100,+100,-100,+100,-100,+100) ;
-
-///////////////////////////// Get the Simulation curves 
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 1, "X"), 22.5, -44.8);
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 1, "X"), 45.0, -44.8);
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 1, "X"), 67.5, -44.8);
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), 90+22.5, -44.8);
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), 90+45.0, -44.8);
-SimBxManager->DrawPolarOffset("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), 90+67.5, -44.8);
-SimBxManager->DrawCartesianFixedY("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), 20,-44.8) ; 
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -20,-44.8) ;
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -30,-44.8) ;
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -40,-44.8) ;
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -50,-44.8) ;
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -60,-44.8) ;
-SimBxManager->DrawCartesianFixedX("Bx", 100, true, ExpPoint->GetOffsetDirection("B", 2, "X"), -60,-44.8) ;
-
-//By 
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Y"), 22.5, -44.8);
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Y"), 45.0, -44.8);
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Y"), 67.5, -44.8);
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Y"), 90+22.5, -44.8);
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Y"), 90+45.0, -44.8);
-SimByManager->DrawPolarOffset("By", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Y"), 90+67.5, -44.8);
-SimByManager->DrawCartesianFixedY("By", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Y"), 20,-44.8) ; 
-SimByManager->DrawCartesianFixedX("By", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Y"), 20,-44.8) ;
-
-//Bz
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Z"), 22.5, -44.8);
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Z"), 45.0, -44.8);
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 1, "Z"), 67.5, -44.8);
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Z"), 90+22.5, -44.8);
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Z"), 90+45.0, -44.8);
-SimBzManager->DrawPolarOffset("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Z"), 90+67.5, -44.8);
-SimBzManager->DrawCartesianFixedY("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Z"), 20,-44.8) ; 
-SimBzManager->DrawCartesianFixedX("Bz", 100, true, ExpPoint->GetOffsetDirection("B", 2, "Z"), 20,-44.8) ;
-
 //2D
+outputFile.cd(); 
 //Bx
 SimBxManager->Draw2DHist ("Bx",200,-100,+100,-100,+100,-44.8) ;  
 //By
@@ -372,7 +274,6 @@ SimBzManager->Draw2DHist ("Bz",200,-100,+100,-100,+100,-44.8) ;
 SimBmagManager->Draw2DHist("Bmag",500,-100,+100,-100,+100,-44.8);
 SimBtanManager->Draw2DHist("Btan",500,-100,+100,-100,+100,-44.8);
 SimBdifManager->Draw2DHist("Bdif",500,-100,+100,-100,+100,-44.8);
-*/
 
 //Close the file on disk
 outputFile.Close();
